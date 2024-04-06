5c8a922fa36bacc92fa62807aa1ad36a5a808156
Dannyrios226 <106358636+Dannyrios226@users.noreply.github.com>
2024-04-06 16:51:22 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,9 +30,10 @@ const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/
 
 // This is an array of strings (TV show titles)
 let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+    "Chest",
+    "Pull",
+    "Legs"
+    "Cardio
 ];
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
