f4dd8b42e5c335be664d85b4ad111b814de1c89d
Dannyrios226 <106358636+Dannyrios226@users.noreply.github.com>
2024-04-06 16:52:48 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ let titles = [
     "Chest",
     "Pull",
     "Legs"
-    "Cardio
+    "Cardio"
 ];
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
