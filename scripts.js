8abd9f675ba8069ba263dbcbc9a1308cace4895d
Dannyrios226 <106358636+Dannyrios226@users.noreply.github.com>
2024-04-07 17:53:11 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -191,9 +191,23 @@ function removeLastCard() {
     showCards(); // Call showCards again to refresh
 }
 
- 
+function getRandomExercises(categoryExercises) {
+    let randomExercises = [];
+    for (let subcategory in categoryExercises) {
+        const exercises = categoryExercises[subcategory];
+        const randomIndex = Math.floor(Math.random() * exercises.length);
+        randomExercises.push(exercises[randomIndex]);
+    }
+    return randomExercises;
+}
 
 
+document.querySelectorAll(".random-workout-btn").forEach(button => {
+    const category = button.getAttribute("data-category");
+    button.addEventListener("click", () => {
+        displayRandomWorkout(category);
+    });
+});
 
 //Write One minute timer button code 
 function oneMinuteRest(){
