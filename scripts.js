fe888ed5379a67e3480dca7fb67d8cb0d8c9035b
Dannyrios226 <106358636+Dannyrios226@users.noreply.github.com>
2024-04-08 22:46:34 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,32 +12,8 @@
  * 10. add an option for workouts that you can do at home 
  * 11. add info on workouts and what to do for them maybe a link to a short youtube video explaining what to do 
  * 12. fix alert only showing a limited amount of Lines 
-
-
- 
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console" 
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
  */
+
 const PUSH_URL = "https://m.media-amazon.com/images/I/71d3CdMuJSL._AC_SY879_.jpg";
 const PULL_URL = "https://m.media-amazon.com/images/I/71A2z2zdGSL._AC_SY879_.jpg";
 const LEGS_URL = "https://m.media-amazon.com/images/I/71bU71azUZL._AC_SY879_.jpg";
@@ -355,14 +331,9 @@ const celebrityWorkoutData = {
     { name: "Battle Ropes", sets: "3", reps: "30 seconds work, 30 seconds rest" },
   ]
 };
-
-
-
-
 //====================================================================
 
 // END OF DATA
-
 
 //====================================================================
 
